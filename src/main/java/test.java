4acfd3cb82e64bbfefa470672225e72d9f407c1a
Reinis Ozolins <reinisozolins@mail.com>
2018-12-12 22:13:23 +0200

    Second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("lol");
         System.out.println("hi");
+        System.out.println("commit changes");
     }
 }
